c713b5309fa633d0cf8f9449f4777d3a91292cd6
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-06-23 18:59:07 -0500

    Update init.sh

--- controller/init.sh
+++ controller/init.sh
@@ -54,4 +54,57 @@ mysql --execute="GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED
 
 apt install -y keystone
 
+## Modify the the file: /etc/keystone/keystone.conf 
+sed -i '/\#connection = <None>/c connection = mysql+pymysql://keystone:keystone@controller/keystone' /etc/keystone/keystone.conf
+sed -i "2842s/\#provider = fernet/provider = fernet/g" /etc/keystone/keystone.conf
 
+## Populate de identity service database
+/bin/sh -c "keystone-manage db_sync" keystone
+
+## Adding repositories
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+
+keystone-manage bootstrap --bootstrap-password keystone \
+--bootstrap-admin-url http://controller:35357/v3/ \
+--bootstrap-internal-url http://controller:5000/v3/ \
+--bootstrap-public-url http://controller:5000/v3/ \
+--bootstrap-region-id RegionOne
+
+## Configure the HTTP SERVER
+
+sed -i -e 's/#ServerName www.example.com/ServerName controller/g' /etc/apache2/sites-enabled/000-default.conf
+service apache2 restart
+rm -f /var/lib/keystone/keystone.db
+
+## Configure administration account
+
+echo 'export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=keystone
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2' > /etc/profile.d/admin-openrc.sh
+
+# Create domain, projects, users and roles
+
+## Create the Service Project
+#openstack project create --domain default --description "Service Project" service
+
+## Create Demo Project
+
+#openstack project create --domain default --description "Demo Project" demo
+
+## Create Demo User
+
+#openstack user create --domain default --password-prompt demo
+
+## Create User rol
+
+#openstack role create user
+
+## Add the user role to the demo project and user
+
+#openstack role add --project demo --user demo user

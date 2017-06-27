283bb0ccc4a5954f8366cda9348ef7b33e92420f
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-06-26 19:22:14 -0500

    updating values

--- controller/init.sh
+++ controller/init.sh
@@ -88,24 +88,52 @@ export OS_AUTH_URL=http://controller:35357/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2' > /etc/profile.d/admin-openrc.sh
 
+## Configure demo account
+
+echo 'export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=demo
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2' > /etc/profile.d/demo-openrc.sh
+
+
+source /etc/profile.d/admin-openrc.sh
+
 # Create domain, projects, users and roles
 
 ## Create the Service Project
-#openstack project create --domain default --description "Service Project" service
+openstack project create --domain default --description "Service Project" service
 
 ## Create Demo Project
 
-#openstack project create --domain default --description "Demo Project" demo
+openstack project create --domain default --description "Demo Project" demo
 
 ## Create Demo User
 
-#openstack user create --domain default --password-prompt demo
+openstack user create --domain default --password demo demo
 
 ## Create User rol
 
-#openstack role create user
+openstack role create user
 
 ## Add the user role to the demo project and user
 
-#openstack role add --project demo --user demo user
+openstack role add --project demo --user demo user
+
+## As admin Request an authentication token:
+
+openstack --os-auth-url http://controller:35357/v3 \
+--os-project-domain-name default --os-user-domain-name default \
+--os-project-name admin --os-username admin token issue
+
+## As demo user Request an authentiction token:
+
+source /etc/profile.d/demo-openrc.sh
+
+openstack --os-auth-url http://controller:5000/v3 \
+--os-project-domain-name default --os-user-domain-name default \
+--os-project-name demo --os-username demo token issue
 

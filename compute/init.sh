ead44a8d225111b0b54588c880ae2d23eb7774cd
Roberto Villarejo Martinez <robertovillarejo@outlook.com>
2017-08-18 19:18:47 -0500

    Compute service (nova)

--- compute/init.sh
+++ compute/init.sh
@@ -15,4 +15,56 @@ sed -i '/# NTP server./a server controller iburst' /etc/chrony/chrony.conf
 sed -i '/#allow ::/a allow 10.0.0.0/24' /etc/chrony/chrony.conf
 service chrony restart
 
+## Install Open Stack repository
+apt install -y software-properties-common
+add-apt-repository cloud-archive:ocata -y
+apt update && apt dist-upgrade -y
+##apt install -y python-openstackclient
 
+##Install compute service
+apt install -y nova-compute
+
+##/etc/nova/nova.conf
+##[DEFAULT]
+sed -i '/#transport_url=<None>/c transport_url = rabbit://openstack:openstack_pass@controller' /etc/nova/nova.conf
+sed -i '/#my_ip=10.222.99.93/c my_ip = 10.0.0.31' /etc/nova/nova.conf
+sed -i '/#use_neutron=true/c use_neutron = True' /etc/nova/nova.conf
+sed -i '/#firewall_driver=<None>/c firewall_driver = nova.virt.firewall.NoopFirewallDriver' /etc/nova/nova.conf
+##[api]
+sed -i '/#auth_strategy=keystone/c auth_strategy = keystone' /etc/nova/nova.conf
+##[keystone_authtoken]
+sed -i '/#auth_uri=<None>/c auth_uri = http://controller:5000' /etc/nova/nova.conf
+sed -i "5610i auth_url = http://controller:35357" /etc/nova/nova.conf
+sed -i '/#memcached_servers=<None>/c memcached_servers = controller:11211' /etc/nova/nova.conf
+sed -i "5610i auth_type = password" /etc/nova/nova.conf
+sed -i "5610i project_domain_name = default" /etc/nova/nova.conf
+sed -i "5610i user_domain_name = default" /etc/nova/nova.conf
+sed -i "5610i project_name = service" /etc/nova/nova.conf
+sed -i "5610i username = nova" /etc/nova/nova.conf
+sed -i "5610i password = nova" /etc/nova/nova.conf
+##[vnc]
+sed -i '/#enabled=true/c enabled = True' /etc/nova/nova.conf
+sed -i '/#vncserver_listen=127.0.0.1/c vncserver_listen = 0.0.0.0' /etc/nova/nova.conf
+sed -i '/#vncserver_proxyclient_address=127.0.0.1/c vncserver_proxyclient_address = $my_ip' /etc/nova/nova.conf
+sed -i "s|#novncproxy_base_url=http://127.0.0.1:6080/vnc_auto.html|novncproxy_base_url = http://controller:6080/vnc_auto.html|" /etc/nova/nova.conf
+##[glance]
+sed -i '/#api_servers=<None>/c api_servers = http://controller:9292' /etc/nova/nova.conf
+##[oslo_concurrency]
+sed -i "s|lock_path=/var/lock/nova|lock_path = /var/lib/nova/tmp|" /etc/nova/nova.conf
+##[placement]
+sed -i '/os_region_name = openstack/c os_region_name = RegionOne' /etc/nova/nova.conf
+sed -i "8179i project_domain_name = Default" /etc/nova/nova.conf
+sed -i "8173i project_name = service" /etc/nova/nova.conf
+sed -i "8155i auth_type = password" /etc/nova/nova.conf
+sed -i "8208i user_domain_name = Default" /etc/nova/nova.conf
+sed -i "8162i auth_url = http://controller:35357/v3" /etc/nova/nova.conf
+sed -i "8203i username = placement" /etc/nova/nova.conf
+sed -i "8213i password = placement" /etc/nova/nova.conf
+
+##Determine whether your compute node supports hardware acceleration for virtual machines
+set hardware_acceleration=$(egrep -c '(vmx|svm)' /proc/cpuinfo)
+if [ "$hardware_acceleration" == "0" ]; then
+  sed -i "s|virt_type=kvm|virt_type=qemu|" /etc/nova/nova-compute.conf
+fi
+
+service nova-compute restart
--- controller/init.sh
+++ controller/init.sh
@@ -206,3 +206,106 @@ openstack image create "cirros" \
   --disk-format qcow2 --container-format bare \
   --public
   openstack image list
+
+  ## Compute service installation
+  mysql --execute="CREATE DATABASE nova_api;"
+  mysql --execute="CREATE DATABASE nova;"
+  mysql --execute="CREATE DATABASE nova_cell0;"
+
+  mysql --execute="GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY 'nova';"
+  mysql --execute="GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY 'nova';"
+  mysql --execute="GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'nova';"
+  mysql --execute="GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'nova';"
+  mysql --execute="GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'localhost' IDENTIFIED BY 'nova';"
+  mysql --execute="GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'%' IDENTIFIED BY 'nova';"
+
+  source /etc/profile.d/admin-openrc.sh
+  openstack user create --domain default --password nova nova
+  openstack role add --project service --user nova admin
+  openstack service create --name nova \
+    --description "OpenStack Compute" compute
+  openstack endpoint create --region RegionOne \
+    compute public http://controller:8774/v2.1
+  openstack endpoint create --region RegionOne \
+    compute internal http://controller:8774/v2.1
+  openstack endpoint create --region RegionOne \
+    compute admin http://controller:8774/v2.1
+  openstack user create --domain default --password placement placement
+  openstack role add --project service --user placement admin
+  openstack service create --name placement --description "Placement API" placement
+  openstack endpoint create --region RegionOne placement public http://controller:8778
+  openstack endpoint create --region RegionOne placement internal http://controller:8778
+  openstack endpoint create --region RegionOne placement admin http://controller:8778
+  apt install -y nova-api nova-conductor nova-consoleauth \
+    nova-novncproxy nova-scheduler nova-placement-api
+
+  ##/etc/nova/nova.conf
+  ##[api_database]
+  sed -i "s|connection=sqlite:////var/lib/nova/nova.sqlite|connection=mysql+pymysql://nova:nova@controller/nova_api|" /etc/nova/nova.conf
+  ##sed -i "3379i connection=mysql+pymysql://nova:nova@controller/nova_api" /etc/nova/nova.conf
+  ## BORRAR EL VALOR ANTERIOR
+  ## sed -i '/\connection=sqlite:////var/lib/nova/nova.sqlite/c connection=mysql+pymysql://nova:nova@controller/nova_api' /etc/nova/nova.conf
+  ##[database]
+  sed -i '/\#connection=<None>/c mysql+pymysql://nova:nova@controller/nova' /etc/nova/nova.conf
+  ##[DEFAULT]
+  sed -i '/\#transport_url=<None>/c transport_url=rabbit://openstack:openstack_pass@controller' /etc/nova/nova.conf
+  ##[api]
+  sed -i '/\#auth_strategy=keystone/c auth_strategy=keystone' /etc/nova/nova.conf
+  ##[keystone_authtoken]
+  sed -i '/\#auth_uri=<None>/c auth_uri=http://controller:5000' /etc/nova/nova.conf
+  ##REVISAR!!!!!!!!!!
+  sed -i "5610i auth_url  = http://controller:35357" /etc/nova/nova.conf
+  sed -i '/\#memcached_servers=<None>/c memcached_servers=controller:11211' /etc/nova/nova.conf
+  sed -i "5800i auth_type  = password" /etc/nova/nova.conf
+  sed -i "5805i project_domain_name = default" /etc/nova/nova.conf
+  sed -i "5805i user_domain_name = default" /etc/nova/nova.conf
+  sed -i "5805i project_name = service" /etc/nova/nova.conf
+  sed -i "5805i username = nova" /etc/nova/nova.conf
+  sed -i "5805i password = nova" /etc/nova/nova.conf
+  ##[DEFAULT]
+  sed -i '/\#my_ip=10.222.99.93/c my_ip=10.0.0.11' /etc/nova/nova.conf
+  sed -i '/\#use_neutron=true/c use_neutron=True' /etc/nova/nova.conf
+  sed -i '/\#firewall_driver=<None>/c firewall_driver=nova.virt.firewall.NoopFirewallDriver' /etc/nova/nova.conf
+  ##[vnc]
+  sed -i '/\#enabled=true/c enabled=true' /etc/nova/nova.conf
+  sed -i '/\#vncserver_listen=127.0.0.1/c vncserver_listen=$my_ip' /etc/nova/nova.conf
+  sed -i '/\#vncserver_proxyclient_address=127.0.0.1/c #vncserver_proxyclient_address=$my_ip' /etc/nova/nova.conf
+  ##[glance]
+  sed -i '/\#api_servers=<None>/c #api_servers=http://controller:9292' /etc/nova/nova.conf
+  ##[oslo_concurrency]
+  sed -i "s|lock_path=/var/lock/nova|lock_path=/var/lib/nova/tmp|" /etc/nova/nova.conf
+  ##[placement]
+  sed -i '/\os_region_name = openstack/c os_region_name = RegionOne' /etc/nova/nova.conf
+  sed -i "8172i project_domain_name = Default" /etc/nova/nova.conf
+  sed -i "8166i project_name = service" /etc/nova/nova.conf
+  sed -i "8148i auth_type = password" /etc/nova/nova.conf
+  sed -i "8198i user_domain_name = Default" /etc/nova/nova.conf
+  sed -i "8154i auth_url = http://controller:35357/v3" /etc/nova/nova.conf
+  sed -i "8192i username = placement" /etc/nova/nova.conf
+  sed -i "8201i password = placement" /etc/nova/nova.conf
+
+  ## Populating the nova databases
+  /bin/sh -c "nova-manage api_db sync" nova
+  /bin/sh -c "nova-manage cell_v2 map_cell0" nova
+  /bin/sh -c "nova-manage cell_v2 create_cell --name=cell1 --verbose" nova
+  /bin/sh -c "nova-manage db sync" nova
+
+  ##Verify nova cell0 and cell1 are registered correctly
+  nova-manage cell_v2 list_cells
+
+  service nova-api restart
+  service nova-consoleauth restart
+  service nova-scheduler restart
+  service nova-conductor restart
+  service nova-novncproxy restart
+
+  ## Add the compute node to the cell database
+  source /etc/profile.d/admin-openrc.sh
+  openstack hypervisor list
+
+  ##Verify operation
+  source /etc/profile.d/admin-openrc.sh
+  openstack compute service list
+  openstack catalog list
+  openstack image list
+  nova-status upgrade check
